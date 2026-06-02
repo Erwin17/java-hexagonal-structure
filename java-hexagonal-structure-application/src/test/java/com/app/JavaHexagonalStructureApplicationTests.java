078cb29cfb32b2844a81eac1a97f9078f9fe1486
Erwin Corzo <erwincorzo@MacBook-Pro-de-Erwin.local>
2026-06-02 11:54:15 -0500

    first commit

---
+++ java-hexagonal-structure-application/src/test/java/com/app/JavaHexagonalStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHexagonalStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
