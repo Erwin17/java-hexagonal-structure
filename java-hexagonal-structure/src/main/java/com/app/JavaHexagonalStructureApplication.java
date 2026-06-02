1d6b6666639ee62852000c41c508a1b1eeea8759
Erwin Corzo <erwincorzo@MacBook-Pro-de-Erwin.local>
2026-06-02 12:30:16 -0500

    changes

--- java-hexagonal-structure/src/main/java/com/app/JavaHexagonalStructureApplication.java
+++ java-hexagonal-structure/src/main/java/com/app/JavaHexagonalStructureApplication.java
@@ -8,5 +8,9 @@ public class JavaHexagonalStructureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaHexagonalStructureApplication.class, args);
+
+        IO.println("==============================");
+        IO.println("============Hexagonal=========");
+        IO.println("==============================");
     }
 }
